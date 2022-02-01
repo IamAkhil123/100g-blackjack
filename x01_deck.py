a69e33d3b2756a4dc94c6de345087e232eb79792
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2022-01-31 17:10:56 -0800

    asfds

--- x01_deck.py
+++ x01_deck.py
@@ -4,7 +4,6 @@
 Create a list of cards
 the cards should be denoted with a number or A, J, Q, K, T (for ace, jack, queen, king or ten)
 as well as a suit
-"""
 
 def createDeck():
   ranks = ['2','3','4','5','6','7','8','9','T','J','Q','K','A']
@@ -27,3 +26,5 @@ def main():
   
 if __name__ == "__main__":
   main()
+"""
+
